34443b3e49df63b77e58c43e9f4bc92b45985e11
rgnswg <194875614+rgnswg@users.noreply.github.com>
2025-10-30 18:38:56 -0300

    cambios en generación de website estatico

--- tools/ssg.py
+++ tools/ssg.py
@@ -7,7 +7,7 @@ import json
 
 def generate_media_json_files(media_dir, project_root):
     print("Generating media JSON files...")
-    
+
     memes_dir = os.path.join(media_dir, 'memes')
     audios_dir = os.path.join(media_dir, 'audios')
 
@@ -24,8 +24,15 @@ def generate_media_json_files(media_dir, project_root):
 
     with open(os.path.join(media_dir, 'audios.json'), 'w') as f:
         json.dump(audio_files, f)
-        
+
     print("Finished generating media JSON files.")
+
+def has_markdown_files(directory_path):
+    for _, _, files in os.walk(directory_path):
+        for file in files:
+            if file.endswith('.md'):
+                return True
+    return False
 
 def generate_file_tree_html(base_path, output_html_path, media_dir):
     base_path = os.path.abspath(base_path)
@@ -47,17 +54,17 @@ def generate_file_tree_html(base_path, output_html_path, media_dir):
             for file in files:
                 if file.startswith('.') or not file.lower().endswith(('.png', '.jpg', '.jpeg', '.gif', '.bmp', '.webp')):
                     continue
-                
+
                 input_path = os.path.join(root, file)
                 base_filename, _ = os.path.splitext(file)
                 output_filename = f"{base_filename}.png"
                 output_path = os.path.join(images_output_dir, output_filename)
-                
+
                 try:
                     with Image.open(input_path) as img:
                         if img.mode != 'RGBA':
                             img = img.convert('RGBA')
-                        
+
                         bbox = img.getbbox()
                         if bbox:
                             cropped_img = img.crop(bbox)
@@ -95,7 +102,7 @@ def generate_file_tree_html(base_path, output_html_path, media_dir):
 
     def process_directory_to_html(current_path):
         html_string = '<ul class="folder-content hidden">'
-        
+
         dirs = []
         files = []
         for item in os.listdir(current_path):
@@ -106,15 +113,16 @@ def generate_file_tree_html(base_path, output_html_path, media_dir):
                 dirs.append(item)
             elif os.path.isfile(item_path) and item.endswith('.md'):
                 files.append(item)
-        
+
         dirs.sort()
         files.sort()
 
         for item in dirs:
             item_path = os.path.join(current_path, item)
-            html_string += f'<li class="folder">{item}'
-            html_string += process_directory_to_html(item_path)
-            html_string += '</li>'
+            if has_markdown_files(item_path):
+                html_string += f'<li class="folder">{item}'
+                html_string += process_directory_to_html(item_path)
+                html_string += '</li>'
 
         for item in files:
             item_path = os.path.join(current_path, item)
@@ -124,27 +132,22 @@ def generate_file_tree_html(base_path, output_html_path, media_dir):
             content_with_html_images = process_images_in_content(content)
             final_html = markdown.markdown(content_with_html_images)
             escaped_html = final_html.replace('"', '&quot;')
-            
+
             file_name = os.path.splitext(item)[0]
             html_string += f'<li class="file" data-content="{escaped_html}">{file_name}</li>'
-            
+
         html_string += "</ul>"
         return html_string
 
     root_html = '<ul>'
-    
-    personajes_path = os.path.join(base_path, 'Personajes')
-    if os.path.exists(personajes_path):
-        root_html += '<li class="folder">Personajes'
-        root_html += process_directory_to_html(personajes_path)
-        root_html += '</li>'
 
-    traits_path = os.path.join(base_path, 'Traits')
-    if os.path.exists(traits_path):
-        root_html += '<li class="folder">Traits'
-        root_html += process_directory_to_html(traits_path)
-        root_html += '</li>'
-        
+    for item in sorted(os.listdir(base_path)):
+        item_path = os.path.join(base_path, item)
+        if os.path.isdir(item_path) and not item.startswith('.') and has_markdown_files(item_path):
+            root_html += f'<li class="folder">{item}'
+            root_html += process_directory_to_html(item_path)
+            root_html += '</li>'
+
     root_html += '</ul>'
 
     with open(output_html_path, 'w', encoding='utf-8') as f:
